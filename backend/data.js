728bdd42ce498a1c1fd75c8f4f7e0da0f846f709
sockry12 <971008.ed@gmail.com>
2021-01-22 13:07:11 -0600

    initial commit

---
+++ backend/data.js
@@ -0,0 +1,111 @@
+const data = {
+    products: [
+       {
+           _id:'1',
+           name:'Gravity ',
+           category:'wine',
+           images:'/images/p-3.jpg',
+           price:'130.00',
+           countInStock:0,
+           brand:'wine',
+           rating:'4.5',
+           numReviews:10,
+           descriptions:'Alta Calidad',  
+       },
+       {
+        _id:'2',
+        name:'Gravity Falls',
+        category:'wine',
+        images:'/images/p-4.jpg',
+        price:'140.00',
+        countInStock:11,
+        brand:'wine',
+        rating:'4.5',
+        numReviews:10,
+        descriptions:'Alta Calidad',   
+    },
+    
+    {
+        _id:'3',
+        name:'Falls',
+        category:'wine',
+        images:'/images/p-3.jpg',
+        price:'150.00',
+        countInStock:14,
+        brand:'wine',
+        rating:'4.5',
+        numReviews:10,
+        descriptions:'Alta Calidad',   
+    },
+    
+    {
+        _id:'4',
+        name:'GravFalls',
+        category:'wine',
+        images:'/images/p-3.jpg',
+        price:'160.00',
+        countInStock:0,
+        brand:'wine',
+        rating:'4.5',
+        numReviews:10,
+        descriptions:'Alta Calidad',   
+    },
+    
+    {
+        _id:'5',
+        name:'GravityFalls',
+        category:'wine',
+        images:'/images/p-3.jpg',
+        price:'170.00',
+        countInStock:14,
+        brand:'wine',
+        rating:'4.5',
+        numReviews:10,
+        descriptions:'Alta Calidad',   
+    },
+    {
+        _id:'6',
+        name:'Gravity F',
+        category:'wine',
+        images:'/images/p-3.jpg',
+        price:'180.00',
+        countInStock:15,
+        brand:'wine',
+        rating:'4.5',
+        numReviews:10,
+        descriptions:'Alta Calidad',   
+
+        
+    },
+    {
+        _id:'7',
+        name:'Gravity F',
+        category:'wine',
+        images:'/images/p-3.jpg',
+        price:'200.00',
+        countInStock:15,
+        brand:'wine',
+        rating:'4.5',
+        numReviews:10,
+        descriptions:'Alta Calidad',   
+
+        
+    },
+    {
+        _id:'8',
+        name:'Gravity F',
+        category:'wine',
+        images:'/images/p-3.jpg',
+        price:'210.00',
+        countInStock:15,
+        brand:'wine',
+        rating:'4.5',
+        numReviews:10,
+        descriptions:'Alta Calidad',   
+
+        
+    },
+    ],
+};
+
+export default data;
---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductSreen';
+
+function App() {
+    return ( 
+    <BrowserRouter>
+    <div className = "grid-container" >
+        <header className = "row" >
+        <div >
+        <a className = "brand" href = "/"> The Valor Group </a> </div><div>
+        <a href = "/cart" > Carro </a> <a href = "/signin" > Ingresar </a> </div></header> 
+    <main>
+        <Route path="/product/:id" component={ProductScreen}></Route>
+        <Route path="/" component={HomeScreen} exact></Route>
+      
+
+    </main> 
+        <footer className = "row center" > All right reserved </footer> </div>
+    </BrowserRouter>
+    );
+}
+
+export default App;
