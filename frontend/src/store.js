9c15a52dddfb54497577c7e2e4f915b1772d4f48
sockry12 <971008.ed@gmail.com>
2021-01-23 11:56:38 -0600

    Se importo Products Details

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import {applyMiddleware, createStore,compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 const initialState = {};
 const reducer = combineReducers({
     productList : productListReducer,
+    productDetails:productDetailsReducer, 
 });
 
 
