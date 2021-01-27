000df9727ba90d178f8b68a5b3ef7ae65e6d049f
sockry12 <971008.ed@gmail.com>
2021-01-27 17:04:04 -0600

    Add cart screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductSreen';
 
@@ -12,9 +13,10 @@ function App() {
         <a className = "brand" href = "/"> The Valor Group </a> </div><div>
         <a href = "/cart" > Carro </a> <a href = "/signin" > Ingresar </a> </div></header> 
     <main>
+        <Route path="/cart/:id?" component={CartScreen}></Route>
         <Route path="/product/:id" component={ProductScreen}></Route>
         <Route path="/" component={HomeScreen} exact></Route>
-      
+        
 
     </main> 
         <footer className = "row center" > All right reserved </footer> </div>
